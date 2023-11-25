e2d17726956fc7df2de909f17cbf06fb34eb3d29
filip.vanelslande@gmail.com <filip.vanelslande@gmail.com>
2023-11-25 19:44:02 +0100

    first commit - build the RAW structure

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+// Some Questions
+
+const quizData = [
+  {
+    question: "The largest circular storm in our solar system is on the surface of which of the following planets?",
+    choices: ["Jupiter", "Venus", "Uranus", "Earth"],
+    answer: "JUPITER"
+  },
+  {
+    question: "The rapidly moving stream of charged particles that is being driven away from the sun is known as what?",
+    choices: ["Star Dust", "Chared Particles", "Solar Wind", "Cosmic Rays"],
+    answer: "SOLAR WIND"
+  }
+  // Add more questions here
+];
+
+// question display
+
+function showQuestion() {
+ 
+}
+
+function checkAnswer(event) {
+ 
+}
+
+function showScore() {
+ 
+}
+
+function nextQuestion() {
+ 
+}
+
+showQuestion();
+
